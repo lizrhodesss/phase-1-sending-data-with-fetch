65c4bda63229ef44d03c34561eea5c0ab9c7418d
liz <rhodes.liz@gmail.com>
2022-07-25 16:48:36 -0400

    commit tree bac774bb38b8f5f70f3bd707eab325b97f88d8fb

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+function submitData(name, email) {
+    fetch ("http://localhost:3000/users", {
+     method: "POST",
+     headers: {
+        "Content-Type": "application/json",
+        "Accept": "applocation/json",
+    },
+    body: JSON.stringify({
+        name: name,
+        email: email        
+    })
+})
+    .then(resp => resp.json())
+    .then(console.log)
+    .catch(error => console.log(error.message))
+}
+//submitData('Todd', 'todd@gmail.com')
